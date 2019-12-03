2813ea48336c7c56fdd57b786be22dbe52bf50ab
André Moreira Souza <msouza.andre@hotmail.com>
2019-12-03 12:23:41 +0200

    Reformatted and adapted visualization code

--- assignment3_mc/softplot/globaldata.h
+++ assignment3_mc/softplot/globaldata.h
@@ -7,49 +7,46 @@
 #define globaldata_h
 
 #include <stdio.h>
+#include <string.h>
 
-struct object_struct
-    {
-    int color;
-    float x;
-    float y;
-    float R;
-    };
+struct object_struct {
+  int color;
+  float x;
+  float y;
+  float R;
+};
 
-struct global_struct
-    {
-    
-    //OpenGl window size
-    int Windowsize_x;           //window size in pixels x direction
-    int Windowsize_y;           //window size in pixels y direction
-    
-    //System Size
-    double SX;
-    double SY;
-    
-    //Sytem Zoom in
-    float zoom_x0,zoom_y0;         //lower left corner
-    float zoom_x1,zoom_y1;         //upper right corner
-    float zoom_deltax,zoom_deltay; //size of the zoomed area
-    
-    float radius_particle;
-    float radius_vertex;
-    
-    char moviefilename[100];
-    FILE *moviefile;
-    
-    int N_frames;
-    int current_frame;
-    
-    int trajectories_on;
-    int trace_length;
+struct global_struct {
+  // OpenGl window size
+  int Windowsize_x;  // window size in pixels x direction
+  int Windowsize_y;  // window size in pixels y direction
 
-    int N_objects;
-    
-    //all objects in the movie file
-    struct object_struct **objects;
+  // System Size
+  double SX;
+  double SY;
 
-    };
+  // Sytem Zoom in
+  float zoom_x0, zoom_y0;          // lower left corner
+  float zoom_x1, zoom_y1;          // upper right corner
+  float zoom_deltax, zoom_deltay;  // size of the zoomed area
+
+  float radius_particle;
+  float radius_vertex;
+
+  char moviefilename[100];
+  FILE *moviefile;
+
+  int N_frames;
+  int current_frame;
+
+  int trajectories_on;
+  int trace_length;
+
+  int N_objects;
+
+  // all objects in the movie file
+  struct object_struct **objects;
+};
 
 extern struct global_struct global;
 
